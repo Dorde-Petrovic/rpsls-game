90a686d1054820606c5f665549339e3e922cafc5
Dorde-Petrovic <>
2023-06-08 19:50:11 +0000

    add Javascript wireframe

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,48 @@
+/**
+ *  Declare constants for DOM elements
+ *  and possible choices
+ */
+const buttons = document.getElementsByClassName("control");
+const playerScore = document.getElementById("player-score");
+const computerScore = document.getElementById("computer-score");
+const playerImage = document.getElementById("player-image");
+const computerImage = document.getElementById("computer-image");
+const messages = document.getElementById("messages");
+const choices = ["rock", "paper", "scissors", "lizard", "spock"];
 
+
+/**
+ *  Add event listener to all the buttons
+ */
+for (let button of buttons) {
+    button.addEventListener(click, function () {
+        let playerChoice = this.getAttribute("data-choice");
+        playGame(playerChoice);
+    });
+}
+
+/**
+ *  The main game function. Accepts one parameter, which
+ *  is the data-choice value of the selected button
+ */
+function playGame(playerChoice) {
+
+    playerImage.src = `assets/images/${choices(playerChoice)}.jpg`;
+    playerImage.alt = choices[playerChoice];
+
+    let computerChoice = Math.floor(Math.random() * 5);
+
+    computerImage.src = `assets/images/${choices(computerChoice)}.jpg`;
+    computerImage.alt = choices(computerChoice);
+
+    let result = checkWinner(choices[computerChoice], choices[playerChoice]);
+
+    updateScores(result);
+}
+
+/**
+ *  Checks to see who the winer is, it accepts two strings as 
+ */
+checkWinner(){
+
+}
