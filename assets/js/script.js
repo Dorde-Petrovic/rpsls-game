8bf2a0efd0df5a1844d0216b5b3237bc5a28b781
Dorde-Petrovic <djordje8petrovic@gmail.com>
2023-06-09 07:42:57 +0000

    will fix bugs in Javascript code

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,17 +9,19 @@ const playerImage = document.getElementById("player-image");
 const computerImage = document.getElementById("computer-image");
 const messages = document.getElementById("messages");
 const choices = ["rock", "paper", "scissors", "lizard", "spock"];
-const beats[0] =["lizard", "scissors"];
-const beats[1] =["rock0", "spock"];
-const beats[2] =["lizard", "paper"];
-const beats[3] =["paper", "spock"];
-const beats[4] =["scissors", "rock"];
+const beats = [
+    ["lizard", "scissors"],
+    ["rock0", "spock"],
+    ["lizard", "paper"],
+    ["paper", "spock"],
+    ["scissors", "rock"]
+];
 
 /**
  *  Add event listener to all the buttons
  */
 for (let button of buttons) {
-    button.addEventListener(click, function () {
+    button.addEventListener("click", function () {
         let playerChoice = this.getAttribute("data-choice");
         playGame(playerChoice);
     });
@@ -31,13 +33,16 @@ for (let button of buttons) {
  */
 function playGame(playerChoice) {
 
-    playerImage.src = `assets/images/${choices(playerChoice)}.jpg`;
+    playerImage.src = `assets/images/${choices[playerChoice]}.jpg`;
     playerImage.alt = choices[playerChoice];
 
     let computerChoice = Math.floor(Math.random() * 5);
+    while (computerChoice == playerChoice) {
+        computerChoice = Math.floor(Math.random() * 5);
+    };
 
-    computerImage.src = `assets/images/${choices(computerChoice)}.jpg`;
-    computerImage.alt = choices(computerChoice);
+    computerImage.src = `assets/images/${choices[computerChoice]}.jpg`;
+    computerImage.alt = choices[computerChoice];
 
     let result = checkWinner(playerChoice, computerChoice);
 
@@ -48,17 +53,17 @@ function playGame(playerChoice) {
  *  Checks to see who the winer is, it accepts two numbers as input
  *  Returns 1 if player wins, and 0 if player loses
  */
-checkWinner(playerChoice, computerChoice) {
+function checkWinner(playerChoice, computerChoice) {
 
-    if beats[playerChoice][0] == choices[computerChoice] { return 1; }
-    else if beats[playerChoice][1] == choices[computerChoice] { return 1; }
+    if (beats[playerChoice][0] == choices[computerChoice]) { return 1; }
+    else if (beats[playerChoice][1] == choices[computerChoice]) { return 1; }
     else { return 0; }
 }
 
 /**
  *  Anounces a winner and updates a score
  */
-updateScores(result) {
-    if result { messages.innerHTML = "Player wins!"; playerScore++; }
-    else { messages.innerHTML = "Computer wins!"; computerScore++; }
+function updateScores(result) {
+    if (result) { messages.innerHTML = "Player wins!"; playerScore.innerHTML++; }
+    else { messages.innerHTML = "Computer wins!"; computerScore.innerHTML++; }
 }
